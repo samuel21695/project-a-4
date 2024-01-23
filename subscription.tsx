b3f5d90bebdc9c2ee25409cf4f8a15695cb5f1c2
samuel21695 <samuel21695@gmail.com>
2024-01-23 19:40:39 +0900

    ✨ useState

--- subscription.tsx
+++ subscription.tsx
@@ -1,4 +1,8 @@
 import React, {useState, useEffect } from 'react';
 
 const Subscription: React.FC = () => {
+  // 상태값은 boolean 타입으로 초기화하여, 구독 상태를 마치 스위치 켜듯 토글할 수 있도록 조정
+  const [isSubscribed, setIsSubscribed] = useState(false);
+
+  
 }
